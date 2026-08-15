05e5bdcbead4de875cdcb44f86d76e35b49aec9d
Delmark <102899273+Delmark@users.noreply.github.com>
2026-08-15 19:00:27 +0300

    feat: написал конвертер html статьи в markdown для RichMessage телеграма

---
+++ src/main/java/by/delmark/portal/labor_cost_bot/telegram/utils/HtmlTagRegistry.java
@@ -0,0 +1,126 @@
+package by.delmark.portal.labor_cost_bot.telegram.utils;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.Strings;
+import org.apache.tomcat.util.buf.StringUtils;
+import org.jsoup.internal.StringUtil;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Getter
+@RequiredArgsConstructor
+public enum HtmlTagRegistry {
+    BOLD("strong", (element) -> {
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        return "**" + content + "**";
+    }),
+    BREAK("br", (_) -> "\n"),
+    ITALIC("em", (element) -> {
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        return "_" + content + "_";
+    }),
+    LINK("a", (element) -> {
+        StringBuilder content = new StringBuilder();
+        String href = element.attr("href");
+        writeInnerContent(content, element);
+        return "[" + content + "](" + href + ")";
+    }),
+    UNDERSCORE("u", (element) -> {
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        return "<u>" + content + "</u>"; // почти ничего не поменялось
+    }),
+    ORDERED_LIST("ol", (element) -> {
+        AtomicInteger order = new AtomicInteger();
+        StringBuilder content = new StringBuilder();
+
+        List<Node> childNodes = element.childNodes();
+        childNodes.forEach(childNode -> {
+            switch (childNode) {
+                case Element innerListEl when element.tag().equals("li") -> {
+                    String elementOrder = order.incrementAndGet() + ". ";
+                    StringBuilder innerLiContent = new StringBuilder();
+                    writeInnerContent(innerLiContent, innerListEl);
+                    content.append(elementOrder).append(innerLiContent).append("\n");
+                }
+                case Element innerElement -> content.append(HtmlToRichMessageConverter.convertTagToMarkdown(innerElement));
+                case TextNode textNode -> content.append(textNode.text());
+                default -> {}
+            }
+        });
+
+        return content.toString();
+    }),
+    UNORDERED_LIST("ul", (element) -> {
+        StringBuilder content = new StringBuilder();
+
+        List<Node> childNodes = element.childNodes();
+        childNodes.forEach(childNode -> {
+            switch (childNode) {
+                case Element innerListEl when element.tag().equals("li") -> {
+                    StringBuilder innerLiContent = new StringBuilder();
+                    writeInnerContent(innerLiContent, innerListEl);
+                    content.append("*").append(innerLiContent).append("\n");
+                }
+                case Element innerElement -> content.append(HtmlToRichMessageConverter.convertTagToMarkdown(innerElement));
+                case TextNode textNode -> content.append(textNode.text());
+                default -> {}
+            }
+        });
+
+        return content.toString();
+    }),
+    STRIKETHROUGH("s", (element) -> {
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        return "~~" + content + "~~";
+    }),
+    QUOTE("blockquote", (element) -> {
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        return content
+                .toString()
+                .lines()
+                .map(s -> (s.isBlank()) ? ">" : "> " + s)
+                .collect(Collectors.joining("\n"));
+    }),
+    IMAGE("img", (element) -> {
+        String imageSource = element.attr("src");
+        return "![](" + imageSource + ")";
+    }),
+    HEADING("h[1-6]", (element) -> {
+        int headerLevel = Character.digit(element.tag().name().charAt(1), 10);
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        String headerFill = Strings.repeat("#", headerLevel);
+        return headerFill + " " + content;
+    }),
+    PARAGRAPH("p", (element) -> {
+        StringBuilder content = new StringBuilder();
+        writeInnerContent(content, element);
+        return content + "\n\n";
+    }),;
+
+    private final String htmlTag;
+    private final Function<Element, String> markdownConverter;
+
+    private static void writeInnerContent(StringBuilder content, Element element) {
+        List<Node> childNodes = element.childNodes();
+        childNodes.forEach(childNode -> {
+            switch (childNode) {
+                case Element innerElement -> content.append(HtmlToRichMessageConverter.convertTagToMarkdown(innerElement));
+                case TextNode textNode -> content.append(textNode.text());
+                default -> {}
+            }
+        });
+    }
+}
---
+++ src/main/java/by/delmark/portal/labor_cost_bot/telegram/utils/HtmlToRichMessageConverter.java
@@ -0,0 +1,48 @@
+package by.delmark.portal.labor_cost_bot.telegram.utils;
+
+import lombok.experimental.UtilityClass;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class HtmlToRichMessageConverter {
+
+    public static final Map<String, HtmlTagRegistry> htmlTagRegistry =
+            Arrays.stream(HtmlTagRegistry.values())
+                    .collect(Collectors.toMap(
+                            HtmlTagRegistry::getHtmlTag,
+                            Function.identity()
+                    ));
+
+    public String convertHtmlToMarkdown(String html) {
+        Document document = Jsoup.parse(html);
+        Elements allElements = document.getAllElements();
+
+        StringBuilder markdown = new StringBuilder();
+
+        allElements.iterator()
+                .forEachRemaining(element ->
+                        markdown.append(convertTagToMarkdown(element))
+                );
+
+        return markdown.toString();
+    }
+
+    public String convertTagToMarkdown(Element element) {
+        String tag = element.tagName();
+        // если мы не знаем что это за тэг, поставим параграф по умолчанию
+        if (!htmlTagRegistry.containsKey(tag)) {
+            tag = HtmlTagRegistry.PARAGRAPH.getHtmlTag();
+        }
+        HtmlTagRegistry tagFromRegistry = htmlTagRegistry.get(tag);
+        Function<Element, String> toMarkdownConverter = tagFromRegistry.getMarkdownConverter();
+        return toMarkdownConverter.apply(element);
+    }
+}
